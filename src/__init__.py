c615e3afbc2ab9fda7e660589069261de81fae3b
shofiya2003 <shofiyabootwala@gmail.com>
2024-06-02 16:30:11 +0530

    created blueprint for api and added health check route

--- src/__init__.py
+++ src/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 import os
 from src.config.config import Config
 from dotenv import load_dotenv
+from pymongo import MongoClient
 
 load_dotenv()
 
@@ -10,3 +11,11 @@ app = Flask(__name__)
 config = Config().dev_config
 
 app.env = config.ENV
+
+# connecting to mongodb
+mongo_client = MongoClient(os.environ.get("MONGO_URI"))
+db = mongo_client["being"]
+
+# import api blueprint to register it with app
+from src.routes import api
+app.register_blueprint(api, url_prefix = "/api")
---
+++ src/routes.py
@@ -0,0 +1,3 @@
+from flask import Blueprint, Request, Response, json
+
+api = Blueprint('api',__name__)
